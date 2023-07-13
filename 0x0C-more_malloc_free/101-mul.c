b4c964930a844ca528ce03da492dea361139e05b
Tumiegh <tumieghthipe62@gmail.com>
2023-07-13 07:33:34 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -12,13 +12,13 @@
  */
 int is_digit(char *s)
 {
-	int a = 0;
+	int i = 0;
 
-	while (s[a])
+	while (s[i])
 	{
-		if (s[a] < '0' || s[a] > '9')
+		if (s[i] < '0' || s[i] > '9')
 			return (0);
-		a++;
+		i++;
 	}
 	return (1);
 }
@@ -31,13 +31,13 @@ int is_digit(char *s)
  */
 int _strlen(char *s)
 {
-	int a = 0;
+	int i = 0;
 
-	while (s[a] != '\0')
+	while (s[i] != '\0')
 	{
-		a++;
+		i++;
 	}
-	return (a);
+	return (i);
 }
 
 /**
@@ -59,7 +59,7 @@ void errors(void)
 int main(int argc, char *argv[])
 {
 	char *s1, *s2;
-	int len1, len2, len, a, carry, digit1, digit2, *result, a = 0;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
 	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
@@ -70,8 +70,8 @@ int main(int argc, char *argv[])
 	result = malloc(sizeof(int) * len);
 	if (!result)
 		return (1);
-	for (a = 0; a <= len1 + len2; a++)
-		result[a] = 0;
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
 	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
 		digit1 = s1[len1] - '0';
@@ -86,14 +86,14 @@ int main(int argc, char *argv[])
 		if (carry > 0)
 			result[len1 + len2 + 1] += carry;
 	}
-	for (a = 0; a < len - 1; a++)
+	for (i = 0; i < len - 1; i++)
 	{
-		if (result[a])
-			b = 1;
-		if (b)
-			_putchar(result[a] + '0');
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
 	}
-	if (!b)
+	if (!a)
 		_putchar('0');
 	_putchar('\n');
 	free(result);
