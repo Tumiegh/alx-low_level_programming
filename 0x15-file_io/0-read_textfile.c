d25b4bdc594da3944846b54c5fc0b9ac1f6c822b
Tumiegh <tumieghthipe62@gmail.com>
2023-11-27 07:50:35 -0800

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,33 +1,36 @@
 #include "main.h"
 
 /**
- * create_file - Creates a file.
- * @filename: A pointer to the name of the file to create.
- * @text_content: A pointer to a string to write to the file.
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
  *
- * Return: If the function fails - -1.
- *         Otherwise - 1.
+ * Return: numbers of letters printed. It fails, returns 0.
  */
-int create_file(const char *filename, char *text_content)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int o, w, len = 0;
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
 
-	if (filename == NULL)
-		return (-1);
+	if (!filename)
+		return (0);
 
-	if (text_content != NULL)
-	{
-		for (len = 0; text_content[len];)
-			len++;
-	}
+	fd = open(filename, O_RDONLY);
 
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(o, text_content, len);
+	if (fd == -1)
+		return (0);
 
-	if (o == -1 || w == -1)
-		return (-1);
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
 
-	close(o);
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
 
-	return (1);
+	close(fd);
+
+	free(buf);
+
+	return (nwr);
 }
