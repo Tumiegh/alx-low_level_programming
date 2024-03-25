e696c6fc5ea97b91e24ebe35d5c690cdd3cae187
Tumiegh <tumieghthipe62@gmail.com>
2024-03-25 16:59:31 -0700

    libdynamic.so

---
+++ 0x18-dynamic_libraries/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
+ * Return: Always 0 (Success)
+ */
+char *_strchr(char *s, char c)
+{
+	int j = 0;
+
+	for (; s[j] >= '\0'; j++)
+	{
+		if (s[j] == c)
+			return (&s[j]);
+	}
+	return (0);
+}
