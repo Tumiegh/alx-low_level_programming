62c2da4c45fb3039fbd69520a8fdfc1a422b065b
Tumiegh <tumieghthipe62@gmail.com>
2024-03-25 17:44:14 -0700

    100-operations.so

---
+++ 0x18-dynamic_libraries/sum.c
@@ -0,0 +1,57 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * add - add two intergers
+ * @a: first int
+ * @b: second int
+ * Return: Addition
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - sub two intergers
+ * @a: first int
+ * @b: second int
+ * Return: Subtraction
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - mul two intergers
+ * @a: first int
+ * @b: second int
+ * Return: Multiply
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * mod - mod two intergers
+ * @a: first int
+ * @b: second int
+ * Return: Mod
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
+
+/**
+ * divi - divi two intergers
+ * @a: first int
+ * @b: second int
+ * Return: Divide
+ */
+int divi(int a, int b)
+{
+	return (a / b);
+}
